ef507124818d0ec34addc7d50174bee824d1d34b
Abi692005 <144250508+Abi692005@users.noreply.github.com>
2023-09-21 17:36:09 +0530

    Students.py

---
+++ Students.py
@@ -0,0 +1,18 @@
+def my Func(e);
+  return e ['CGPA']
+def sort_students(list):
+  list.sort(key=my Func,reverse = true)
+  print(list)
+  students = [
+    {'name':'x','rollNo':'001','CGPA':86.9},
+    {'name':'Y','rollNo':'002','CGPA':55.6},
+    {'name':'Z','rollNo':'003','CGPA':90.01},
+    {'name':'A',:rollNo':'004','CGPA':60.78},
+     ]
+  students.append({'name':'B','rollNo':'005,CGPA':76.43})
+
+
+sort_students(students)
+
+
+
